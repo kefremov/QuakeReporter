9aa76f1daa60c3bd9477b663f085b069f716eb8c
kefremov <k-efremov@hotmail.com>
2017-06-27 04:34:17 +0200

    commit

---
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,34 @@
+package com.example.android.quakereport;
+
+
+
+public class Earthquake {
+
+    private String mLocation;
+    private long mDate;
+    private double mMagnitude;
+    private String mUrl;
+
+    public Earthquake(double magnitude, String location, long  date,String url){
+        mLocation = location;
+        mDate = date;
+        mMagnitude = magnitude;
+        mUrl = url;
+    }
+
+    public String getLocation(){
+        return mLocation;
+    }
+
+    public long getDate(){
+        return mDate;
+    }
+
+    public double getMagnitude(){
+        return mMagnitude;
+    }
+
+    public String getUrl(){
+        return mUrl;
+    }
+}
